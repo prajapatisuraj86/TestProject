946a2f4773937c036fdc6a582fb4b728b350bc73
Suraj Prajapati <prajapatisuraj86@gmail.com>
2018-04-22 19:23:23 +0530

    TEST Case Commited

--- src/test/java/com/pg/AppTest.java
+++ src/test/java/com/pg/AppTest.java
@@ -4,36 +4,18 @@ import com.pg.App
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import junit.framework.Assert;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+    
+	@Test
     public void testApp()
     {
-        assertTrue( App.calculateSquare(2) == 4);
+		Assert.assertEquals(App.calculateSquare(2) == 4);
     }
 }
